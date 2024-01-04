6cf53043d5ad3b0cd9fd47b3bd2a74eeec83031e
Chinedu Ekene Okpala <allstackdev@gmail.com>
2024-01-04 19:28:59 +0000

    setup aliase

---
+++ src/components/legal/privacy-policy/index.ts
@@ -0,0 +1,10 @@
+export { default as Intro } from "./Intro";
+export { default as SocialSharing } from "./SocialSharing";
+export { default as UpdatesToPrivacy } from "./UpdatesToPrivacy";
+export { default as InformationWeCollect } from "./InformationWeCollect";
+export { default as InformationWithOthers } from "./InformationWithOthers";
+export { default as CollectionPrivacyInfo } from "./CollectionPrivacyInfo";
+export { default as InformationYouProvide } from "./InformationYouProvide";
+export { default as DisclosureOfPersonalInfo } from "./DisclosureOfPersonalInfo";
+export { default as ProtectionOfPersonalInfo } from "./ProtectionOfPersonalInfo";
+export { default as InformationAboutTransaction } from "./InformationAboutTransaction";
