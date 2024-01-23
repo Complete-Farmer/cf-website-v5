7925202c07481ca05f6ba0d53e8f1a38ba67c7f3
Chinedu Ekene Okpala <allstackdev@gmail.com>
2024-01-23 12:20:02 +0000

    update astro env

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,9 +5,6 @@ interface ImportMetaEnv {
   readonly BOT_PASS: string;
   readonly BOT_EMAIL: string;
   readonly PUBLIC_ENV: "DEV" | "PROD";
-  readonly PUBLIC_BUYER_SERVICE: string;
-  readonly PUBLIC_GROWER_SERVICE: string;
-  readonly PUBLIC_VENDOR_SERVICE: string;
   readonly PUBLIC_FACEBOOK_PIXEL_ID: string;
   readonly PUBLIC_PRISM_ACCESS_TOKEN: string;
   readonly PUBLIC_GLOBAL_SITE_TAG_ID: string;
